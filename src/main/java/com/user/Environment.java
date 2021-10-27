e57be78cb2a9caeea2abd8392da42a5605acc843
Hachard Victor <31635811+VictorHachard@users.noreply.github.com>
2021-10-27 13:24:32 +0200

    Update Environment.java

--- src/main/java/com/user/Environment.java
+++ src/main/java/com/user/Environment.java
@@ -12,6 +12,6 @@ public class Environment {
 
     //Prod
     //public static final String DATA_FOLDER = "/home/ubuntu/data/images/";
-    //public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(Arrays.asList("http://141.94.251.48", "http://vps.ypc.yt, "http://www.vps.ypc.yt"));
+    //public static final List<String> ACCESS_CONTROL_ALLOW_ORIGIN_URL = new ArrayList<>(Arrays.asList("http://141.94.251.48", "http://vps.ypc.yt", "http://www.vps.ypc.yt"));
 
 }
